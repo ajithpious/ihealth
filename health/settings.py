c4a5d622e68ef5dc8ab20576d78fee8cf7836320
Ajith pious <ajithpiousolive@gmail.com>
2022-04-17 18:37:39 +0100

    addding heroku settings

--- health/settings.py
+++ health/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django settings
+
+import django_heroku
+django_heroku.settings(locals())
