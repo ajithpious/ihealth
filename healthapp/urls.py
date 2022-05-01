65f3cdf701d3100305a8c8c0ffaa13e7bd0707d1
Ajith pious <ajithpiousolive@gmail.com>
2022-05-01 19:33:41 +0100

    adding api for esp32

--- healthapp/urls.py
+++ healthapp/urls.py
@@ -1,9 +1,11 @@
-from .views import getdata, home, patient, patients
+from .views import getpulse, getspo2, gettemp, home, patient, patients
 from django.urls import path
 
 
 urlpatterns = [ path('',home),
 path("patients",patients),
 path("patient1",patient),
-path("getdata",getdata)
+path("temp",gettemp),
+path("pulse",getpulse),
+path("spo2",getspo2)
 ]
--- healthapp/views.py
+++ healthapp/views.py
@@ -30,8 +30,18 @@ def patient(request):
     graph=fig.to_html()
     context={"graph":graph}
     return render(request,"patient.html",context=context)
-def getdata(request):
+def gettemp(request):
     url="https://industrial.api.ubidots.com/api/v1.6/devices/esp32/temp/values/"
+    params={"page_size":1,"token":"BBFF-Bfw8lO95m9pzKw8zE0Zupzw5OrKDQ0"}
+    r = requests.get(url = url, params = params)
+    return HttpResponse(r,content_type="application/json")
+def getpulse(request):
+    url="https://industrial.api.ubidots.com/api/v1.6/devices/esp32/bpm/values/"
+    params={"page_size":1,"token":"BBFF-Bfw8lO95m9pzKw8zE0Zupzw5OrKDQ0"}
+    r = requests.get(url = url, params = params)
+    return HttpResponse(r,content_type="application/json")
+def getspo2(request):
+    url="https://industrial.api.ubidots.com/api/v1.6/devices/esp32/spo2/values/"
     params={"page_size":1,"token":"BBFF-Bfw8lO95m9pzKw8zE0Zupzw5OrKDQ0"}
     r = requests.get(url = url, params = params)
     return HttpResponse(r,content_type="application/json")
